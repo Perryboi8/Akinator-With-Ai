0fdde59fe96c4ab5dff83b4998de16c0492f4fc1
Perryboi8 <perryboi125@gmail.com>
2024-09-25 14:00:05 -0700

    Added comments for the django website to make it more readeable

--- Django-Website-Akinator/polls/admin.py
+++ Django-Website-Akinator/polls/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+#Where we can add our apps to be editable on the admin site
 # Register your models here.
--- Django-Website-Akinator/polls/apps.py
+++ Django-Website-Akinator/polls/apps.py
@@ -1,5 +1,4 @@
-from django.apps import AppConfig
-
+from django.apps import AppCon
 
 class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
