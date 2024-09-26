d4b9d83d0e695c0051045640e6d55e9830e78d8d
Perryboi8 <perryboi125@gmail.com>
2024-09-25 18:39:42 -0700

    Updating

--- Django-Website-Akinator/polls/urls.py
+++ Django-Website-Akinator/polls/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     path("home", views.home_page),
-    path("question", views.question_page),
+    path("question", views.question_page, name = 'question_page')
 
 ]
--- Django-Website-Akinator/polls/views.py
+++ Django-Website-Akinator/polls/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home_page(request):
+
   return render(request, 'home.html')
 
 def question_page(request):
+  if (request.method =="POST"):
+    user_response = request.POST.get('response')
+
+    if user_response =='yes':
+      print("The user selected Yes")
+    elif user_response == 'no':
+      print("The user selected No")
+    elif user_response == 'idk':
+      print("the user selected idk")
+
+    return redirect('question_page')
+
   return render(request, 'index.html')
