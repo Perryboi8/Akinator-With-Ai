9747061dbf1938690504e89a73bdfc016b1a24fb
Perryboi8 <perryboi125@gmail.com>
2024-11-19 21:32:19 -0800

    Added a button to switch between 2 Ai Models

--- Django-Website-Akinator/polls/urls.py
+++ Django-Website-Akinator/polls/urls.py
@@ -8,8 +8,9 @@ app_name = "polls"
 #This is a continuation of the home page so it would be like url.home/___
 urlpatterns = [
     path('', views.home_page, name= 'home_page'),
-    path('question', views.question_view, name='question_page'),
+    path('question-gpt', views.question_view_gpt, name='question_page_gpt'),
     path('get-answers', views.get_answers, name='get_page'),
     path('clear-answers', views.clear_answers, name='clear_page'),
-    path('guess', views.guess_page, name='guessPage')
+    path('guess', views.guess_page, name='guessPage'),
+    path('question-llama', views.question_view_llama, name = 'question_page_llama')
 ]
--- Django-Website-Akinator/polls/views.py
+++ Django-Website-Akinator/polls/views.py
@@ -7,7 +7,7 @@ from .gpt4_chara import Genre, get_initial_question, get_next_question
 def home_page(request):
   return render(request, 'home.html')
 
-def question_view(request):
+def question_view_gpt(request):
   if request.method == 'POST':
 
     button_answer = request.POST.get('button-answer') #Grabs the button answer from index.html
@@ -50,6 +50,8 @@ def get_answers(request):
 def clear_answers(request):
    request.session.clear()
    return HttpResponse(f'Answers Are Now Cleared')
+
+
 def guess_page(request):
    #place holder for the guess page to show it, You can add code here
 
@@ -57,3 +59,38 @@ def guess_page(request):
 
 
    return render(request, 'guess.html')
+
+def question_view_llama(request):
+  if request.method == 'POST':
+
+    button_answer = request.POST.get('button-answer') #Grabs the button answer from index.html
+    textbox_answer = request.POST.get('textbox-answer') #Grabs Textbox answer from html
+
+    answers = request.session.get('answers', [])
+    previous_questions = request.session.get('previous_questions', [])
+
+
+    if textbox_answer and textbox_answer.strip():
+       answers.append(textbox_answer.strip())
+    elif button_answer:
+       answers.append(button_answer)
+
+    print(answers)
+    request.session['answers'] = answers
+
+    log = "\n".join(answers)
+
+
+    while True:
+      question = get_next_question(log)
+
+      if question not in previous_questions:
+         previous_questions.append(question)
+         break
+
+    request.session['previous_questions'] = previous_questions
+
+    return render(request, 'index2.html', {'ai_response': question, 'answers': answers})
+
+
+  return render(request, 'index2.html')
